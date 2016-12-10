f69e1a6c2933d2a3ad7ec0ebd37eafab62679f07
sirdir <phddir@gmail.com>
2016-12-10 02:55:02 +0200

    Revert "override service, add some "this" for doPostin services()"
    This reverts commit 911d5ace66d59ae14299d61fb9bc760a57be0439.

--- src/main/java/HelloForm.java
+++ src/main/java/HelloForm.java
@@ -1,8 +1,6 @@
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import java.io.*;
-import java.text.MessageFormat;
-import java.util.ResourceBundle;
 
 /**
  * Created by sirdir on 03.12.16.
@@ -36,94 +34,6 @@ public class HelloForm extends HttpServlet {
                 "</ul>\n" +
                 "</body></html>");
     }
-
-    private static final String METHOD_DELETE = "DELETE";
-    private static final String METHOD_HEAD = "HEAD";
-    private static final String METHOD_GET = "GET";
-    private static final String METHOD_OPTIONS = "OPTIONS";
-    private static final String METHOD_POST = "POST";
-    private static final String METHOD_PUT = "PUT";
-    private static final String METHOD_TRACE = "TRACE";
-
-    private static final String HEADER_IFMODSINCE = "If-Modified-Since";
-    private static final String HEADER_LASTMOD = "Last-Modified";
-
-
-    private static final String LSTRING_FILE =
-            "javax.servlet.http.LocalStrings";
-    private static ResourceBundle lStrings =
-            ResourceBundle.getBundle(LSTRING_FILE);
-
-
-    private void maybeSetLastModified(HttpServletResponse resp,
-                                      long lastModified) {
-        if (resp.containsHeader(HEADER_LASTMOD))
-            return;
-        if (lastModified >= 0)
-            resp.setDateHeader(HEADER_LASTMOD, lastModified);
-    }
-
-
-    @Override
-    public void service(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException
-    {
-        String method = req.getMethod();
-
-        if (method.equals(METHOD_GET)) {
-            long lastModified = getLastModified(req);
-            if (lastModified == -1) {
-                // servlet doesn't support if-modified-since, no reason
-                // to go through further expensive logic
-                this.doGet(req, resp);
-            } else {
-                long ifModifiedSince = req.getDateHeader(HEADER_IFMODSINCE);
-                if (ifModifiedSince < (lastModified / 1000 * 1000)) {
-                    // If the servlet mod time is later, call doGet()
-                    // Round down to the nearest second for a proper compare
-                    // A ifModifiedSince of -1 will always be less
-                    maybeSetLastModified(resp, lastModified);
-                    this.doGet(req, resp);
-                } else {
-                    resp.setStatus(HttpServletResponse.SC_NOT_MODIFIED);
-                }
-            }
-
-        } else if (method.equals(METHOD_HEAD)) {
-            long lastModified = getLastModified(req);
-            maybeSetLastModified(resp, lastModified);
-            doHead(req, resp);
-
-        } else if (method.equals(METHOD_POST)) {
-            this.doPost(req, resp);
-
-        } else if (method.equals(METHOD_PUT)) {
-            doPut(req, resp);
-
-        } else if (method.equals(METHOD_DELETE)) {
-            doDelete(req, resp);
-
-        } else if (method.equals(METHOD_OPTIONS)) {
-            doOptions(req,resp);
-
-        } else if (method.equals(METHOD_TRACE)) {
-            doTrace(req,resp);
-
-        } else {
-            //
-            // Note that this means NO servlet supports whatever
-            // method was requested, anywhere on this server.
-            //
-
-            String errMsg = lStrings.getString("http.method_not_implemented");
-            Object[] errArgs = new Object[1];
-            errArgs[0] = method;
-            errMsg = MessageFormat.format(errMsg, errArgs);
-
-            resp.sendError(HttpServletResponse.SC_NOT_IMPLEMENTED, errMsg);
-        }
-    }
-
 
     //handle POST
     @Override
